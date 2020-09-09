bc101ce2be4f5e6492aeb54d76532d2ac97437b3
celiojunior <cj2018lima@gmail.com>
2020-09-09 16:01:11 -0300

    Regressive timer finished

---
+++ CELIO VIEIRA DE LIMA JUNIOR/Aula 4 (09-09-2020)/Regressive Time/source/js/script.js
@@ -0,0 +1,108 @@
+class Timer{
+
+    constructor(buttonId = "#timer-button", timerDisplayId = "#timer-display", timerInputId = "#timer-input"){
+        this.options = ["►", "■"];
+        this.counting = false;
+        this.time = 0; // Time input in minutes
+        this.minutes = 0;
+        this.seconds = 0;
+        this.hours   = 0;
+        this.configHtmlDom(buttonId, timerDisplayId, timerInputId);
+    }
+
+    configHtmlDom(buttonId, timerDisplayId, timerInputId){
+        this.button = $(buttonId);
+        this.display = $(timerDisplayId);
+        this.input   = $(timerInputId);
+        this.updateInput();
+        this.updateButton();
+        this.updateDisplay();
+        this.button.click(this.actionStart.bind(this));
+        this.input.keyup(this.setTime.bind(this))
+        this.setTime();
+    }
+
+    actionStart(){
+        if (!this.time > 0){
+            return;
+        }
+        if (this.counting){
+            clearInterval(this.interval);
+            this.counting = false;
+        }else{
+            this.counting = true;
+            this.interval = setInterval(this.updateTimer.bind(this), 1000);
+        }
+        this.makeTimer();
+        this.updateInput();
+        this.updateButton();
+        this.updateDisplay();
+    }
+
+    makeTimer(){
+        this.hours   = (this.time / 60);
+        this.minutes = Math.round((this.hours - parseInt(this.hours))*60);
+        this.seconds = parseInt(this.minutes - parseInt(this.minutes));
+        this.hours = parseInt(this.hours);
+    }
+
+    setTime(){
+        if (parseInt(this.input.val()) > parseInt(this.input.attr("min")) && parseInt(this.input.val()) < parseInt(this.input.attr("max"))){   
+            this.time = this.input.val();
+        }else{
+            this.time = 0;
+        }
+        this.makeTimer();
+        this.updateDisplay();
+    }
+
+    clockLogic(){
+        this.seconds -= 1;
+        if (this.seconds < 0){
+            this.minutes -= 1;
+            this.seconds = 59;            
+        }
+        if (this.minutes < 0){
+            this.minutes = 59;
+            this.hours -= 1;
+        }
+    }
+
+    updateTimer(){
+        this.clockLogic();
+        if (this.hours == 0 && this.minutes == 0 && this.seconds == 0){
+            this.actionStart();
+        }else{
+            this.updateDisplay();
+        }
+     }
+
+     updateInput(){
+         if (this.counting){
+            this.input.attr("disabled", true);
+         }else{
+             this.input.attr("disabled", false);
+         }
+     }
+
+     updateButton(){
+        if(this.counting){
+            this.button.text(this.options[1]);
+        }else{
+            this.button.text(this.options[0]);
+        }
+     }
+
+     updateDisplay(){
+        this.display.text(`${pad(this.hours, 2)}:${pad(this.minutes, 2)}:${pad(this.seconds, 2)}`);
+     }
+}
+
+function pad(n, width) { 
+    /* Roubei esta função do StackOverflow 
+    apenas para embelezar a bagaça.*/
+    n = n + ''; 
+    return n.length >= width ? n :
+        new Array(width - n.length + 1).join('0') + n; 
+}
+
