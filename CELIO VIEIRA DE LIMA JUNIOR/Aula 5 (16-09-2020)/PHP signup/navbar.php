3b9bdc583dced992bc9b89ab77a79542e1ce5fcf
celiojunior <cj2018lima@gmail.com>
2020-09-22 17:13:59 -0300

    App name fixed

--- CELIO VIEIRA DE LIMA JUNIOR/Aula 5 (16-09-2020)/PHP signup/navbar.php
+++ CELIO VIEIRA DE LIMA JUNIOR/Aula 5 (16-09-2020)/PHP signup/navbar.php
@@ -2,7 +2,7 @@
     <link rel="stylesheet" href="./source/css/navbar.css">
     <script src="./source/js/jquery-3.5.1.js"></script>
     <span id="items">
-        <h1>PHP Login</h1>
+        <h1>PHP Sign System</h1>
         <button class="hidden-button" id="menu-button">...</button>
         <span id="panel">
             <a class="hyperlink" href="./">Home</a>
